52ae5d1786326660388ac64dd52e878078b1282d
armands62 <armandsliepa3@gmail.com>
2021-04-20 15:53:35 +0300

    Pop Up

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const PopUp = document.getElementById('popUp');
+
+window.addEventListener('load', () => {
+    document.getElementById('jaunaPrece').addEventListener('click', () => {
+        PopUp.style.display = 'flex';
+    })
+
+})
