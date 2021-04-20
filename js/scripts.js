a83bf42cd848829384f7ecb959ae87c2664548b1
armands62 <armandsliepa3@gmail.com>
2021-04-20 16:46:55 +0300

    Gandriz pabeigts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
 const PopUp = document.getElementById('popUp');
+let preces = [];
 
 window.addEventListener('load', () => {
+    preces = JSON.parse(localStorage.getItem("preces") || "[]");
+        console.log(preces)
+        render();
+    });
+
     document.getElementById('jaunaPrece').addEventListener('click', () => {
         PopUp.style.display = 'flex';
     })
+    document.getElementById('pievienotPreci').addEventListener('click', () => {
+        PopUp.style.display = 'none';
 
+    let prece = {Prece: Prece.value, Daudzums: Daudzums.value};
+
+    Prece.value = "";
+    Daudzums.value = "";
+
+    preces.push(prece);
+
+    render();
 })
+    function render() {
+        let biblioteka = document.getElementById('biblioteka');
+        biblioteka.innerHTML = "";
+    
+        for(let i = 0; i < preces.length; i++) {
+            let prece = `
+            <div class="prece">
+                <h3>Prece: ${preces[i].Prece}</h3> 
+                <h3>Daudzums: ${preces[i].Daudzums}</h3>
+            </div>`;
+    
+            biblioteka.innerHTML += prece;
+        }
+    
+        localStorage.setItem("preces", JSON.stringify(preces))
+    }
