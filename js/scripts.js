3db66086c1ab1090d23a1a14275ffd1485f65caf
armands62 <armandsliepa3@gmail.com>
2021-04-27 13:26:15 +0300

    Animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
-const PopUp = document.getElementById('popUp');
+const PopUp = document.getElementById("popUp");
 let preces = [];
 
-window.addEventListener('load', () => {
-    preces = JSON.parse(localStorage.getItem("preces") || "[]");
-        console.log(preces)
-        render();
-    });
+window.addEventListener("load", () => {
+  preces = JSON.parse(localStorage.getItem("preces") || "[]");
+  console.log(preces);
+  render();
+});
 
-    document.getElementById('jaunaPrece').addEventListener('click', () => {
-        PopUp.style.display = 'flex';
-    })
-    document.getElementById('pievienotPreci').addEventListener('click', () => {
-        PopUp.style.display = 'none';
+document.getElementById("jaunaPrece").addEventListener("click", () => {
+  PopUp.style.display = "flex";
+});
+document.getElementById("pievienotPreci").addEventListener("click", () => {
+  PopUp.style.display = "none";
 
-    let prece = {Prece: Prece.value, Daudzums: Daudzums.value};
+  let prece = { Prece: Prece.value, Daudzums: Daudzums.value };
 
-    Prece.value = "";
-    Daudzums.value = "";
+  Prece.value = "";
+  Daudzums.value = "";
 
-    preces.push(prece);
+  preces.push(prece);
 
-    render();
-})
-    function render() {
-        let biblioteka = document.getElementById('biblioteka');
-        biblioteka.innerHTML = "";
-    
-        for(let i = 0; i < preces.length; i++) {
-            let prece = `
+  render();
+});
+function render() {
+  let biblioteka = document.getElementById("biblioteka");
+  biblioteka.innerHTML = "";
+
+  for (let i = 0; i < preces.length; i++) {
+    let prece = `
             <div class="prece">
                 <h3>Prece: ${preces[i].Prece}</h3> 
                 <h3>Daudzums: ${preces[i].Daudzums}</h3>
             </div>`;
-    
-            biblioteka.innerHTML += prece;
-        }
-    
-        localStorage.setItem("preces", JSON.stringify(preces))
-    }
+
+    biblioteka.innerHTML += prece;
+  }
+
+  localStorage.setItem("preces", JSON.stringify(preces));
+}
